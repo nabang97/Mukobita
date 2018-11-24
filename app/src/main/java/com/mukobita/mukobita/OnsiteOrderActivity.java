ebf45b6a3182953b51de01f712397e5e05cb04c1
nabang97 <parawitayolanda@gmail.com>
2018-11-24 15:46:59 +0700

    first commit

---
+++ app/src/main/java/com/mukobita/mukobita/OnsiteOrderActivity.java
@@ -0,0 +1,13 @@
+package com.mukobita.mukobita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnsiteOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onsite_order);
+    }
+}
